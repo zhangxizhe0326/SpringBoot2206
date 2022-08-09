565c6af18105d5050758841ccfe313542a3dd5e5
14162 <1416289525@qq.com>
2022-08-09 15:24:52 +0800

    one

---
+++ src/main/java/com/tedu/stringboot2206/StringBoot2206Application.java
@@ -0,0 +1,13 @@
+package com.tedu.stringboot2206;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBoot2206Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBoot2206Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tedu/stringboot2206/StringBoot2206ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tedu.stringboot2206;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringBoot2206ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
